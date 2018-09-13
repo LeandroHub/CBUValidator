c93c2a0857c29c82d8bc632cd988dc539bf9a72c
Leandro Fournier <leandro.fournier@gmail.com>
2018-09-13 17:11:03 -0300

    Initial commit

---
+++ Example/Pods/Target Support Files/CBUValidator/CBUValidator-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_CBUValidator : NSObject
+@end
+@implementation PodsDummy_CBUValidator
+@end
---
+++ Example/Pods/Target Support Files/Pods-CBUValidator_Example/Pods-CBUValidator_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CBUValidator_Example : NSObject
+@end
+@implementation PodsDummy_Pods_CBUValidator_Example
+@end
